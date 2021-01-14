e1672951531d2e973f124a887b73d533ec32a692
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2021-01-13 23:45:19 -0300

    implement html header

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import iconSearch from './assets/icon-search.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-header-div">
+          <div className="App-header-div-logo">
+            <p className="App-header-div-logo-texto">VITRINE</p>
+            <p className="App-header-div-logo-subTexto">PRODUTOS E SERVIÇOS</p>
+          </div>
+          <div className="App-header-div-pesquisa">
+            <div className="App-header-div-pesquisa-cardInput">
+              <input
+                className="App-header-div-pesquisa-input"
+                placeholder="Buscar produtos, serviços e muito mais..."
+              />
+              <div className="App-header-div-pesquisa-divisao"></div>
+              <div className="App-header-div-pesquisa-btn" onClick={() => {
+                alert('Em desenvolvimento')
+              }}>
+                <img className="App-header-div-pesquisa-btn-icon" src={iconSearch} alt="icon-search" />
+              </div>
+            </div>
+          </div>
+          <div className="App-header-div-acesso">
+            <button className="App-header-div-acesso-btn" onClick={() => alert("Crie sua conta")}>Crie sua conta</button>
+            <button className="App-header-div-acesso-btn" onClick={() => alert("Entre")}>Entre</button>
+          </div>
+        </div>
       </header>
     </div>
   );
